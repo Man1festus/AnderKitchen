b00feb73f718c722ba97c3d97df7cc57fd4e46df
Man1festus <lopek2013@gmai.com>
2016-10-18 02:34:44 +0300

    18.10.2016

--- js/data.js
+++ js/data.js
@@ -1,19 +1,16 @@
 "use strict"
 var Icebox = (function() {
-	var products =[{
+	var products = [{
 	"name":"молоко",
-	"imgSrc":"url('img/milk.png')",
-	"possibleRec":['омлет','блины','сырники']
+	"imgSrc":"url('img/milk.png')"
 },
 {
 	"name":"мука",
-	"imgSrc":"url('img/flour.png')",
-	"possibleRec":['пироги','блины','пирожки','сырники']
+	"imgSrc":"url('img/flour.png')"
 },
 {
 	"name":"яйца",
-	"imgSrc":"url('img/egg.png')",
-	"possibleRec":['омлет','сырники','блины','пирожки','печеньки']
+	"imgSrc":"url('img/egg.png')"
 },
 {
 	"name":"блины",
@@ -25,22 +22,39 @@ var Icebox = (function() {
 },
 {
 	"name":"сыр",
-	"imgSrc":"url('img/cheese.png')",
-	"possibleRec":['омлет','бутерброд','пицца']
+	"imgSrc":"url('img/cheese.png')"
 },
 {
 	"name":"сахар",
-	"imgSrc":"url('img/sugar.png')",
-	"possibleRec":['сырники']
+	"imgSrc":"url('img/sugar.png')"
 },
 {
 	"name":"творог",
-	"imgSrc":"url('img/cottage_cheese.png')",
-	"possibleRec":['сырники']
+	"imgSrc":"url('img/cottage_cheese.png')"
 },
 {
 	"name":"омлет",
-	"imgSrc":"url('img/omelet.png')",
+	"imgSrc":"url('img/omelet.png')"
+}];
+
+var recipes = [{
+	"name":"блины",
+	"rec":['молоко','яйца','мука']
+
+},
+{
+	"name":"сырники",
+	"rec":['яйца','мука','молоко','сахар','творог']
+},
+{
+	"name":"омлет",
+	"rec":['яйца','молоко','сыр']
+
+},
+{
+	"name":"пицца",
+	"rec":['основа пиццы','кетчуп','помидоры','колбаса']
+
 }];
 
 function getProducts(){
@@ -49,6 +63,52 @@ function getProducts(){
 		masProducts.push(products[i].name);
 	}
 	return masProducts;
+}
+
+function altOutRec(arrProd){
+	var result = '';
+
+	var obj = {};
+	for (var i = 0; i < arrProd.length; i++) {
+		var str = arrProd[i];
+	    obj[str] = true;
+		}
+	for(var key in recipes){
+		var count = 0;
+		var strRec = recipes[key].rec;
+		for (var i = 0; i < strRec.length; i++) {
+			if(obj[strRec[i]]){
+				count++;
+			}
+		if(count == arrProd.length){
+	    	result += recipes[key].name;
+		}
+		}
+		count = 0;
+	}
+	return result;
+}
+
+function possibleRecReturner(productList){
+	var regularRec = [];
+		for(var key in recipes){
+			for (var i = 0; i < productList.length; i++) {
+				if((recipes[key].rec.toString().split(',')).indexOf(productList[i].toLowerCase()) != -1){
+				regularRec.push(''+recipes[key].name);
+				regularRec = regularRec.toString().split(',');
+				break;
+				}
+			}
+		}
+	  var obj = {};
+
+	for (var i = 0; i < regularRec.length; i++) {
+		var str = regularRec[i];
+	    obj[str] = true;
+		}
+	  regularRec = [];
+	return(Object.keys(obj).toString().split(','));
+
 }
 function outImgSrc(prodName){
 	var images = '';
@@ -61,7 +121,10 @@ function outImgSrc(prodName){
 	}
 	return images;
 }
-function outRecipes(arrProd){
+
+
+/*function outRecipes(arrProd){ // Старый способ поиска рецепта. Искал выходной продукт по наибольшему совпадению продуктов для него на столе. Более гибкий но не 
+								// всегда точный.
 		var regularRec = [],
 		result = '',
 		maxPut = 0;
@@ -87,11 +150,13 @@ function outRecipes(arrProd){
 			} 
 		}
 		return result;
-}
+}*/
 	return {
 		getProducts : getProducts,
 		outImgSrc : outImgSrc,
-		outRecipes: outRecipes
+		/*outRecipes: outRecipes,*/
+		possibleRecReturner: possibleRecReturner,
+		altOutRec: altOutRec
 	};
 })();
 
